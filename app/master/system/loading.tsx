5730c4662ebfdb273bfdd94596974888bdcff906
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-13 03:19:03 +0800

    feat: add instant loading state for master system

---
+++ app/master/system/loading.tsx
@@ -0,0 +1,5 @@
+import ControlPanelSkeleton from '../../../components/control/ControlPanelSkeleton';
+
+export default function Loading() {
+  return <ControlPanelSkeleton label="Loading system health" />;
+}
