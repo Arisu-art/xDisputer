a07f6ca9369c91223bc4fbdaba0f452d37095d90
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-14 21:39:02 +0800

    style: hide raw file inputs in manager upload actions

--- components/ManagerTemplateWorkspaceChrome.tsx
+++ components/ManagerTemplateWorkspaceChrome.tsx
@@ -98,16 +98,17 @@ export default function ManagerTemplateWorkspaceChrome() {
       background: #020617;
       border-color: #020617;
     }
+    .manager-upload-action input[type=file],
     .manager-upload-input {
-      position: absolute;
-      width: 1px;
-      height: 1px;
-      overflow: hidden;
-      clip: rect(0 0 0 0);
-      white-space: nowrap;
-      border: 0;
-      padding: 0;
-      margin: -1px;
+      position: absolute !important;
+      width: 1px !important;
+      height: 1px !important;
+      overflow: hidden !important;
+      clip: rect(0 0 0 0) !important;
+      white-space: nowrap !important;
+      border: 0 !important;
+      padding: 0 !important;
+      margin: -1px !important;
     }
     .manager-template-direct-actions .remove-node {
       min-height: 38px;
