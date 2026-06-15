00152783bba68872b9a016051a33b5a363707786
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-15 11:21:04 +0800

    feat: add manager workspace contracts panel

---
+++ app/manager-workspace/contracts/page.tsx
@@ -0,0 +1,5 @@
+import ConsolePanelRoutePage from '../../../components/console/ConsolePanelRoutePage';
+
+export default function ManagerWorkspaceContractsPage() {
+  return <ConsolePanelRoutePage route="/manager-workspace/contracts" />;
+}
