a5d5b2a34478c124a78bea3c1f4a8b9c3cfd5d1b
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-15 11:22:14 +0800

    feat: add manager workspace automation route

---
+++ app/manager-workspace/automation/page.tsx
@@ -0,0 +1,5 @@
+import ConsolePanelRoutePage from '../../../components/console/ConsolePanelRoutePage';
+
+export default function ManagerWorkspaceAutomationPanelPage() {
+  return <ConsolePanelRoutePage route="/manager-workspace/automation" />;
+}
