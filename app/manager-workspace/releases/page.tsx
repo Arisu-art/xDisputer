58a1c24e56b10b8d63de25842ba8cfb753ac92d6
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-15 11:21:39 +0800

    feat: add manager workspace releases panel

---
+++ app/manager-workspace/releases/page.tsx
@@ -0,0 +1,5 @@
+import ConsolePanelRoutePage from '../../../components/console/ConsolePanelRoutePage';
+
+export default function ManagerWorkspaceReleasesPage() {
+  return <ConsolePanelRoutePage route="/manager-workspace/releases" />;
+}
