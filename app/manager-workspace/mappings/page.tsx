31e4ce585b8bab78c24da517ee77cafc7136cb34
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-15 11:21:18 +0800

    feat: add manager workspace mappings panel

---
+++ app/manager-workspace/mappings/page.tsx
@@ -0,0 +1,5 @@
+import ConsolePanelRoutePage from '../../../components/console/ConsolePanelRoutePage';
+
+export default function ManagerWorkspaceMappingsPage() {
+  return <ConsolePanelRoutePage route="/manager-workspace/mappings" />;
+}
