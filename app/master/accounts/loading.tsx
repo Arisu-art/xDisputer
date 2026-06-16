f14bdac456597619e073025ecf84555f9a34c274
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-17 05:42:04 +0800

    Use instant master accounts loading shell

--- app/master/accounts/loading.tsx
+++ app/master/accounts/loading.tsx
@@ -1,5 +1,5 @@
-import ControlPanelSkeleton from '../../../components/control/ControlPanelSkeleton';
+import ConsoleInstantLoading from '../../../components/console/ConsoleInstantLoading';
 
 export default function MasterAccountsLoading() {
-  return <ControlPanelSkeleton label="Loading master accounts" />;
+  return <ConsoleInstantLoading role="master" title="Workspace account directory." description="Showing the master account shell now while account filters, rows, and pagination load." datasetLabel="Master account dataset" />;
 }
