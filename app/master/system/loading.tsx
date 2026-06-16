46fae09daad408550d03b3a019f8982c22a421ac
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-17 06:01:57 +0800

    Use instant master health loading shell

--- app/master/system/loading.tsx
+++ app/master/system/loading.tsx
@@ -1,5 +1,5 @@
-import ControlPanelSkeleton from '../../../components/control/ControlPanelSkeleton';
+import ConsoleInstantLoading from '../../../components/console/ConsoleInstantLoading';
 
 export default function Loading() {
-  return <ControlPanelSkeleton label="Loading system health" />;
+  return <ConsoleInstantLoading role="master" title="Health overview." description="Showing the master health shell now while observability records load." datasetLabel="Health records" />;
 }
