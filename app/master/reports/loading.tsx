ccabfff73e3a178ab4a4b9e510cfe4cdf492b2e9
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-17 05:52:33 +0800

    Use instant master reports loading shell

--- app/master/reports/loading.tsx
+++ app/master/reports/loading.tsx
@@ -1,5 +1,5 @@
-import ControlPanelSkeleton from '../../../components/control/ControlPanelSkeleton';
+import ConsoleInstantLoading from '../../../components/console/ConsoleInstantLoading';
 
 export default function Loading() {
-  return <ControlPanelSkeleton label="Loading master reports" />;
+  return <ConsoleInstantLoading role="master" title="Governance reports." description="Showing the master reports shell now while report filters and recent activity load." datasetLabel="Master reports" />;
 }
