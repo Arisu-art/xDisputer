e2655a4d3608353feedb7bec41a0643b60d9d1c4
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-20 01:13:16 +0800

    Add global notification dock mount

---
+++ components/notifications/GlobalNotificationDockMount.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import NotificationDock from './NotificationDock';
+
+export default function GlobalNotificationDockMount() {
+  return <div className="global-notification-dock-mount" data-global-notification-dock="true"><NotificationDock /></div>;
+}
