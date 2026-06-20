3d1fa21f225b8d7cc461dfb1be1aeddab03ed4b8
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-21 01:25:06 +0800

    Retire global notification dock mount

--- components/notifications/GlobalNotificationDockMount.tsx
+++ components/notifications/GlobalNotificationDockMount.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import NotificationDock from './NotificationDock';
-
 export default function GlobalNotificationDockMount() {
-  return <div className="global-notification-dock-mount" data-global-notification-dock="true"><NotificationDock /></div>;
+  return null;
 }
