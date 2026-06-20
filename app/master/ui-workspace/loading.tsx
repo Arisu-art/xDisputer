31e3e3b0ab23c8c5db4c92c6716e9348e13b0272
Arisu-art <83152637+Arisu-art@users.noreply.github.com>
2026-06-20 18:56:34 +0800

    Remove master UI workspace loading surface

--- app/master/ui-workspace/loading.tsx
+++
@@ -1,5 +0,0 @@
-import ConsoleInstantLoading from '../../../components/console/ConsoleInstantLoading';
-
-export default function MasterUiWorkspaceLoading() {
-  return <ConsoleInstantLoading role="master" title="UI workspace." description="Loading the master visual governance shell while the workspace model prepares." datasetLabel="UI workspace model" />;
-}
